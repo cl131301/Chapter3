4be1bd198b8e318b02aa7278efd8bd3fb75ccde6
Christine Lewis <33032693+cl131301@users.noreply.github.com>
2017-11-15 10:01:42 -0600

    Updated Stadium Seating

---
+++ Stadium Seating/Stadium Seating/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        lblResult.Text = ""
+        textbox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        LabelA.Text = ""
+        LabelB.Text = ""
+        LabelC.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dblTotalRevenue As Double
+        LabelA.Text = CDbl(textbox1.Text) * 15
+        LabelB.Text = CDbl(TextBox2.Text) * 12
+        LabelC.Text = CDbl(TextBox3.Text) * 9
+        dblTotalRevenue = CDbl(textbox1.Text) * 15 + CDbl(TextBox2.Text) * 12 + CDbl(TextBox3.Text) * 9
+
+        lblResult.Text = dblTotalRevenue.ToString("c")
+    End Sub
+End Class
