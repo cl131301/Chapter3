47849af025989660daa8d5b0b6a76fd661587037
Christine Lewis <33032693+cl131301@users.noreply.github.com>
2017-11-15 13:33:32 -0600

    Updated Taxes

---
+++ Calculating Property Taxes/Calculating Property Taxes/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        lblResult.Text = ""
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dblPropertyValue As Double
+        dblPropertyValue = (CDbl(TextBox1.Text) * 0.6) * 0.0064
+        lblResult.Text = dblPropertyValue.ToString("c")
+    End Sub
+
+End Class
